06c7ff496cf9141d89792c8f86cfecc929f21ea4
Nicolas Villabona <nvillabona97@gmail.com>
2026-02-25 13:11:51 -0500

    update to next 16

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-
+                protocol: "https",
                 hostname: "img.icons8.com"
             }
         ]
